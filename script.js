ce8d9488208ef2440d3572180b88a4b08e93cdb6
Inspire2025 <inspirethailand.official@gmail.com>
2025-05-09 15:12:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.querySelectorAll("button").forEach(button => {
+/*document.querySelectorAll("button").forEach(button => {
     button.addEventListener("click", () => {
         alert("ขอบคุณที่สั่งซื้อ! กรุณาติดต่อ Line: @inspire เพื่อยืนยันการสั่งซื้อค่ะ");
     });
 });
+*/
